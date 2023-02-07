b316cccf1d090d5775f90b993d1b60bfd7cd87fd
Borys Mch <borys.mch@gmail.com>
2023-02-07 06:33:37 +0200

    Created Products grid

---
+++ components/Products.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { data } from "@/utils/data";
+import Image from "next/image";
+import { Grid, GridItem, Card, Box } from "@chakra-ui/react";
+import Link from "next/link";
+
+const Products = () => {
+  return (
+    <div>
+      <Grid templateColumns={{ base: "1fr", lg: "repeat(4, 1fr)" }} gap={6}>
+        {data.products.map((product) => (
+          <GridItem key={product.id} colSpan={1}>
+            <Card>
+              <Link href={`/product/${product.id}`}>
+                <Image
+                  src={`/images${product.img}`}
+                  height={230}
+                  width={230}
+                  style={{
+                    objectFit: "cover",
+                    height: "230px",
+                    margin: "auto",
+                  }}
+                  alt={product.title}
+                />
+                <Box py={1} px={2}>
+                  <h3>{product.title}</h3>
+                  <div>{product.price} UAH</div>
+                </Box>
+              </Link>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default Products;
---
+++ utils/data.js
@@ -0,0 +1,52 @@
+export const data = {
+  products: [
+    {
+      id: 1,
+      title: "Ноутбук Acer Aspire 7 A715-42G-R8BL",
+      descr:
+        "Aspire 7 – компактний ноутбук із якісним дисплеєм Full HD IPS і великим набором інтерфейсів.",
+      price: 45999,
+      img: "/aspire-7.png",
+    },
+    {
+      id: 2,
+      title: 'Ноутбук Apple MacBook Pro 13" M2 256Gb 2022',
+      descr:
+        "Новий чіп M2 робить 13-дюймовий MacBook Pro більш функціональним, ніж будь-коли.",
+      price: 60999,
+      img: "/apple-m2.png",
+    },
+    {
+      id: 3,
+      title: "Ноутбук ASUS Laptop X515EA-BQ2066L",
+      descr:
+        "ASUS Laptop 15 X515 - це універсальний ноутбук початкового рівня, що поєднує у собі стильний дизайн та сучасні технології.",
+      price: 25999,
+      img: "/asus-x515.png",
+    },
+    {
+      id: 4,
+      title: "Фотоапарат Canon EOS M50 Mark II",
+      descr:
+        "Камера EOS M50 Mark II відкриває нові способи для фотографування, відеозйомки, прямих трансляцій та спілкування з фанатами.",
+      price: 27999,
+      img: "/canon-eos-m50.png",
+    },
+    {
+      id: 5,
+      title: "Дзеркальний фотоапарат Nikon D5200 kit 18-55",
+      descr:
+        "Nikon D5200 це справжній подарунок для всіх любителів творчості фото, адже в даній моделі встановлений якісний CMOS сенсор не поступається Nikon D7000.",
+      price: 21500,
+      img: "/nikon-d5200.png",
+    },
+    {
+      id: 6,
+      title: "Фотоапарат Sony Alpha A6400 kit",
+      descr:
+        "Sony Alpha A6400 kit (16-50mm) Black - технологічний компактний фотоапарат зі змінним об'єктивом і байонетом Sony E-mount.",
+      price: 41999,
+      img: "/sony-alpha-a6400.png",
+    },
+  ],
+};
