526d12e8a74c79b103c1ccd7c6929127d631e114
Borys Mch <borys.mch@gmail.com>
2023-02-08 09:46:48 +0200

    Created product model and product api

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    id: { type: String, require: true, unique: true },
+    title: { type: String, require: true },
+    image: { type: String, require: true },
+    price: { type: Number, require: true },
+    descr: { type: String, require: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
+export default Product;
